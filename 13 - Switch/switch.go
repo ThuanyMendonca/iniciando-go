ea95ee3c517f317baa1e9d70a7bc971ae56d4dad
Thuany Mendonça <thuany1997@hotmail.com>
2021-06-02 17:09:58 -0300

    Fundamentos da linguagem

--- 13 - Switch/switch.go
+++ 13 - Switch/switch.go
@@ -56,6 +56,7 @@ func diaDaSemana2(numero int) string {
 }
 
 func main() {
+	// No Go não possui o break, quando a condição é satisfeita
 	fmt.Println("Switch")
 	dia := diaDaSemana(7)
 	fmt.Println(dia)
